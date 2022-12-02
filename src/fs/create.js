1509b6763e7221a87a136ad6c16f36889953b6fe
AnatoliyIliev <vekok12@gmail.com>
2022-12-02 17:19:29 +0200

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile, readdir  } from 'fs/promises';
+import {join,  dirname } from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename); 
+
+const create = async () => {     
+    const pathFile = join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        const files = await readdir(`${__dirname}\\files`);
+        
+        for (const file of files) {
+            if (file === 'fresh.txt') {
+                throw 'FS operation failed';
+            }
+        };
+
+       await writeFile(pathFile, 'I am fresh and young', 'utf8', (err) => { if (err) { throw err } });
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 await create();
