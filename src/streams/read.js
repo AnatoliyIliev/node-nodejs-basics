7d4383611adc0259b053dd8f62419f106414ed1b
AnatoliyIliev <vekok12@gmail.com>
2022-12-04 12:11:34 +0200

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from "fs";
+import { join, dirname } from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathFiles = join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const readFile = createReadStream(pathFiles, 'utf-8');
+      
+    readFile.on("data", (chunk) => {
+        console.log(chunk)
+    });     
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await read();
