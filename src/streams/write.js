f3e32b8b6fe5f981abbb0c486c9d768c942306e1
AnatoliyIliev <vekok12@gmail.com>
2022-12-04 12:44:22 +0200

    feat: implement streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,31 @@
+import { createWriteStream } from "fs";
+import { join, dirname } from "path";
+import { stdin, stdout } from 'process';
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathFiles = join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+    const readFile = createWriteStream(pathFiles, 'utf-8');
+
+    stdout.write(
+        'Hello! Write what you want and press "Strl - С" or "exit" to exit. Thank you.\n',
+    );
+
+    stdin.on('data', data => {
+        if (data.toString().trim() === 'exit') {
+            process.exit();
+        } else {
+            readFile.write(data.toString());
+        }
+    });
+
+    process.on('SIGINT', () => process.exit());
+    process.on('exit', () => {
+        console.log('\nGood luck');
+    });
 };
 
 await write();
