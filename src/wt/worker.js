8b105f22415519fdbc1f4860ec4f7db4a6586cc7
AnatoliyIliev <vekok12@gmail.com>
2022-12-04 22:41:31 +0200

    fix: add Math.random on worker file

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,6 +3,9 @@ import { parentPort, workerData } from 'node:worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+    if (Math.random() > 0.9) {
+        throw new Error(null)
+    }
     parentPort.postMessage(nthFibonacci(workerData))
 };
 
